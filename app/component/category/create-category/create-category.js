38a716912d5aa0f836e99c27a5007a19b659c449
brials <brials008@hotmail.com>
2017-04-14 11:20:20 -0700

    created create category component

---
+++ app/component/category/create-category/create-category.js
@@ -0,0 +1,24 @@
+'use strict';
+
+require('./_create-category.scss');
+
+module.exports = {
+  template: require('./create-category.html'),
+  controller: ['$log', 'categoryService', CreateCategoryController],
+  controllerAs: 'createCategoryCtrl',
+};
+
+function CreateCategoryController($log, categoryService){
+  $log.debug('CreateCategoryController');
+
+  this.category = {};
+
+  this.createCategory = function(){
+    $log.debug('createCategoryCtrl.createCategory');
+    categoryService.createCategory(this.category)
+    .then(() => {
+      this.category.categoryType = null;
+      this.category.desc = null;
+    });
+  };
+}
