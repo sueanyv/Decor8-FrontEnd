ca64a61fc391dff62ae13b069a04ddd06188d415
sueanyv <valenciasue@hotmail.com>
2017-04-17 16:35:03 -0700

    modal and adding files to post

--- app/service/post-service.js
+++ app/service/post-service.js
@@ -1,10 +1,40 @@
 'use strict';
 
-module.exports = ['$log', '$q', '$http', 'authService', postService];
+module.exports = ['$log', '$q', '$http', 'Upload','authService', postService];
 
-function postService($log, $q, $http, authService){
+function postService($log, $q, $http, Upload, authService){
   $log.debug('postService');
 
   let service = {};
-  let service.posts = [];
+  service.posts = [];
+
+  service.createPost = function(postData){
+    $log.debug('commentService.createPost');
+    return authService.getToken()
+    .then( token => {
+      let url = `${__API_URL__}/api/post`;
+      let headers = {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json'
+      };
+
+      return Upload.upload({
+        url,
+        headers,
+        method: 'POST',
+        data: {
+          name: postData.name,
+          desc: postData.desc,
+          file: postData.file,
+          price: postData.price,
+          comment: postData.comment
+        }
+      });
+    })
+    .catch( err => {
+      $log.error(err.message);
+      return $q.reject(err);
+    });
+  };
+  return service;
 }
