2793e480974724c1f5d72ef2aea5812c9a0d8c10
caylazabel <caylazabel@gmail.com>
2017-04-17 17:41:51 -0700

    commiting changes for safety

--- app/component/profile/create-profile/create-profile.js
+++ app/component/profile/create-profile/create-profile.js
@@ -4,22 +4,23 @@ require('./_create-profile.scss');
 
 module.exports = {
   template: require('./create-profile.html'),
-  controller: ['$log', 'profileService', CreateProfileController],
+  controller: ['$log', '$location', 'profileService', CreateProfileController],
   controllerAs: 'createProfileCtrl',
 };
 
-function CreateProfileController($log, profileService){
+function CreateProfileController($log, $location, profileService){
   $log.debug('CreateProfileController');
 
   this.profile = {};
 
   this.uploadPic = function() {
-    console.log(this.profile, 'logging profile');
     profileService.uploadProfilePic(this.profile)
-    .then( () => {
+    .then(res => {
       this.profile.name = null;
       this.profile.bio = null;
       this.profile.file = null;
+      console.log('res data', res);
+      $location.url(`/profile/${res._id}`);
     });
   };
 }
