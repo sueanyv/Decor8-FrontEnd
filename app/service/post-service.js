33f74c09d50fa0937951779ccc38da1c7bd07307
brials <brials008@hotmail.com>
2017-04-17 22:19:54 -0700

    post upload working

--- app/service/post-service.js
+++ app/service/post-service.js
@@ -12,7 +12,8 @@ function postService($log, $q, $http, Upload, authService){
     $log.debug('commentService.createPost');
     return authService.getToken()
     .then( token => {
-      let url = `${__API_URL__}/api/post`;
+      console.log('post data', postData)
+      let url = `${__API_URL__}/api/category/${postData.categoryID}/post`; //eslint-disable-line
       let headers = {
         Authorization: `Bearer ${token}`,
         Accept: 'application/json'
@@ -25,13 +26,40 @@ function postService($log, $q, $http, Upload, authService){
         data: {
           name: postData.name,
           desc: postData.desc,
-          file: postData.file,
+          image: postData.image,
           price: postData.price,
           comment: postData.comment
         }
       });
     })
+    .then(res => {
+      return res.data;
+    })
     .catch( err => {
+      $log.error(err.message);
+      return $q.reject(err);
+    });
+  };
+
+  service.fetchPost = function(postId){
+    $log.debug('postService.fetchPost');
+
+    return authService.getToken()
+    .then(token => {
+      let url = `${__API_URL__}/api/post/${postId}`; //eslint-disable-line
+      let config = {
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      };
+      return $http.get(url, config);
+    })
+    .then(res => {
+      $log.log('post created');
+      return res.data;
+    })
+    .catch(err => {
       $log.error(err.message);
       return $q.reject(err);
     });
