b4f34ff953395ffb8df64a0505ec586186037887
brials <brials008@hotmail.com>
2017-04-19 18:03:21 -0700

    changed assets and styled navabr

--- app/view/landing/landing-controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = ['$log', '$location', '$rootScope', 'authService', LandingController];
-
-function LandingController($log, $location, authService){ //eslint-disable-line
-  $log.debug('LandingController');
-  let url = $location.url();
-  this.showSignup = url === '/join' || url === '/signup';
-}
